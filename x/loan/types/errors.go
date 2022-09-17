cbbd473954ec88cf1218f8f65ecd4daed8f142c5
olsenmatthew <olsenmatthew780@gmail.com>
2022-09-17 00:45:39 -0400

    Add liquidate-loan message

--- x/loan/types/errors.go
+++ x/loan/types/errors.go
@@ -8,6 +8,7 @@ import (
 
 // x/loan module sentinel errors
 var (
+	ErrDeadline       = sdkerrors.Register(ModuleName, 3, "deadline")
 	ErrWrongLoanState = sdkerrors.Register(ModuleName, 2, "wrong loan state")
 	ErrSample         = sdkerrors.Register(ModuleName, 1100, "sample error")
 )
